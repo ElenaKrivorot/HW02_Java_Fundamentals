eab6c4a4a90ab181701dc42bc86eaabc9cb578f9
ElenaKrivorot <57360499+ElenaKrivorot@users.noreply.github.com>
2019-12-14 15:48:32 +0300

    Add files via upload

--- src/by/krivorot/hw02/arrays/Task18.java
+++ src/by/krivorot/hw02/arrays/Task18.java
@@ -1,10 +1,42 @@
 package by.krivorot.hw02.arrays;
 
 public class Task18 {
+	/*
+	 * «Суперзамок». Секретный замок для сейфа состоит из 10 расположенных в рад
+	 * ячеек, в которые надо вставить игральные кубики. Но дверь открывается только
+	 * в том случае, когда в любых трех соседних ячейках сумма точек на передних
+	 * гранях кубиков равна 10. (Игральный кубик имеет на каждой грани от 1 до 6
+	 * точек). Напишите программу, которая разгадывает код замка при условии, что
+	 * два кубика уже вставлены в ячейки.
+	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int[] mas = new int[10];
+
+		mas[0] = 5;
+		mas[1] = 3;
+		init(mas);
+		printMas(mas);
+
+	}
+
+	public static void init(int[] mas) {
+		int i;
+
+		for (i = 2; i < mas.length; i++) {
+			mas[i] = 10 - mas[i - 1] - mas[i - 2];
+		}
+
+	}
+
+	public static void printMas(int[] mas) {
+		int i;
+
+		for (i = 0; i < mas.length; i++) {
+			System.out.printf("%3d", mas[i]);
+		}
+		System.out.println();
 	}
 
 }
